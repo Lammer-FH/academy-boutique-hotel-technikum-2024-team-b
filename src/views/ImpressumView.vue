bccb50f35bb1b9445b9bbb9b51e6fc2a989a8089
tamar <tamar.dono@gmail.com>
2024-10-18 18:12:44 +0200

    WIP: Update App.vue and router for Impressum feature

---
+++ src/views/ImpressumView.vue
@@ -0,0 +1,18 @@
+<script>
+import AppImpressum from "@/components/AppImpressum.vue";
+
+export default {
+  name: "ImpressumView",
+  components: {AppImpressum}
+}
+</script>
+
+<template>
+  <main>
+    <AppImpressum />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
