3c7900e68e93f77dc3128054c25117200beeea6d
Magda <pa23l003@technikum-wien.at>
2024-10-18 21:18:44 +0200

    route for gettingHere created and for about adjusted

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GettingHereView from "@/views/GettingHereView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +11,18 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
+      path: '/ueber-uns',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/anreise',
+      name: 'GettingHereView',
+      component: GettingHereView
+    },
   ]
 })
 
