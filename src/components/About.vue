260c8097e85c4b7c0de2ce1bdb5bfa4c0a979164
Magda <pa23l003@technikum-wien.at>
2024-10-18 14:03:36 +0200

    About-component created and in AboutView integrated

---
+++ src/components/About.vue
@@ -0,0 +1,53 @@
+<script>
+
+export default {
+  name: "About",
+}
+</script>
+
+<template>
+  <div class="about">
+    <b-container class="text-center">
+      <b-row class="justify-content-center">
+        <b-col sm="12" md="8" lg="12">
+          <h1>Boutique-Hotel Technikum</h1><br><br>
+
+          <img src="../assets/images/dummy_World’s-Most-Stunning-Hotel-Lobby-Designs.jpg" alt="HotelLobby"
+               class="img-fluid pb-5">
+
+          <p>Willkommen im <b>Boutique-Hotel Technikum</b>, Ihrem urbanen Rückzugsort im Herzen von Wien!
+            Unser charmantes Hotel vereint zeitlose Eleganz mit modernem Komfort und bietet eine einzigartige
+            Atmosphäre,
+            in
+            der Sie sich sofort wohlfühlen werden.</p>
+          <p>Unser Haus befindet sich in bester innerstädtischer Lage, ideal für Gäste, die das historische und
+            kulturelle
+            Wien erkunden möchten.
+            Ob es die Pracht der Ringstraße, der Stephansdom oder die zahlreichen Museen sind – von unserem Hotel aus
+            ist
+            alles bequem zu Fuß oder mit den öffentlichen Verkehrsmitteln erreichbar.</p>
+          <p>Das <b>Technikum</b> ist mehr als nur ein Ort zum Übernachten. Unser Name steht für das Zusammenspiel von
+            Tradition und
+            Innovation:
+            Historische Architektur trifft auf zeitgenössisches Design. Wir legen großen Wert auf eine persönliche und
+            herzliche Betreuung unserer Gäste.
+            Jedes Zimmer ist individuell gestaltet und vereint modernen Komfort mit Wiener Charme, damit Sie sich wie zu
+            Hause
+            fühlen</p>
+          <p>Unser Ziel ist es, Ihren Aufenthalt so angenehm und unvergesslich wie möglich zu gestalten –
+            egal ob Sie geschäftlich oder privat in Wien sind. Lassen Sie sich von der lebendigen Atmosphäre und der
+            Gastfreundschaft unseres Boutique-Hotels begeistern.</p>
+          <p>Haben Sie noch Fragen? Dann kontaktieren Sie uns unter technikum@boutique-hotel.com.</p>
+          <p>Wir freuen uns darauf, Sie bei uns begrüßen zu dürfen!</p><br>
+          <p>Ihr Team vom Boutique-Hotel Technikum</p>
+
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import About from '../components/About.vue';
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <About />
+  </main>
 </template>
+
 
 <style>
 @media (min-width: 1024px) {
