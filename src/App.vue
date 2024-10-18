e824773137238f7975ad191b10eeb6a98466ee47
David Desktop <david_ala@icloud.com>
2024-10-18 21:09:54 +0200

    cleaned up App and added margin top for fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import LandingpageView from "@/views/LandingpageView.vue";
-import {BContainer} from "bootstrap-vue-3";
 import Navbar from "@/components/Navbar.vue";
-import CardsLandingPage from "@/components/CardsLandingPage.vue";
 </script>
 
 <template>
- <Navbar></Navbar>
-  <landingpage-view></landingpage-view>
+  <Navbar></Navbar>
+  <RouterView id="router"/>
 </template>
 
 <style scoped>
+#router{
+  margin-top: 2rem;
+}
 </style>
