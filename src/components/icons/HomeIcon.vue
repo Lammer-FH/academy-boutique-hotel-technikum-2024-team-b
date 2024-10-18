1eb77896dba0a6b4d22df51493596e7675480c50
David Desktop <david_ala@icloud.com>
2024-10-18 21:57:21 +0200

    created HomeIcon for Header

---
+++ src/components/icons/HomeIcon.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "HomeIcon"
+}
+</script>
+
+<template>
+  <img src="../../assets/pictures/logo-technikum-wien-academy-1.png" height="40" width="80" alt="Hotel Logo"></template>
+<style scoped>
+
+</style>
