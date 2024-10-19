f69e6e840319338b072ed1e37824d385b1bd07ef
tamar <tamar.dono@gmail.com>
2024-10-19 11:23:36 +0200

    Finalize changes before switching branches

--- src/components/AppImpressum.vue
+++ src/components/AppImpressum.vue
@@ -1,39 +1,44 @@
-
 <template>
-  <div class="impressum-container">
-    <h1>Impressum</h1>
-    <p>Angaben gemäß § 5 TMG:</p>
-    <p>
-      Technikum Hotel <br>
-      Mariahilfer Straße 123 <br>
-      1060 Wien, Österreich
-    </p>
-    <p>
-      Vertreten durch: <br>
-      Franz Müller
-    </p>
-    <p>
-      Kontakt: <br>
-      Telefon: +43 1 234 567 890 <br>
-      E-Mail: kontakt@technikumhotel.at
-    </p>
-    <p>
-      Registereintrag: <br>
-      Eintragung im Handelsregister. <br>
-      Registernummer: HRB 987654 <br>
-      Registergericht: Handelsgericht Wien
-    </p>
-    <p>
-      Umsatzsteuer-ID: <br>
-      Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz: ATU12345678
-    </p>
-    <p>
-      Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: <br>
-      Franz Müller <br>
-      Mariahilfer Straße 123 <br>
-      1060 Wien, Österreich
-    </p>
-  </div>
+  <b-container fluid class="text-center">
+    <b-row class="justify-content-center">
+      <b-col sm="12" md="8" lg="6">
+        <div class="impressum-container">
+          <h1>Impressum</h1>
+          <p>Angaben gemäß § 5 TMG:</p>
+          <p>
+            Technikum Hotel <br />
+            Mariahilfer Straße 123 <br />
+            1060 Wien, Österreich
+          </p>
+          <p>
+            Vertreten durch: <br />
+            Franz Müller
+          </p>
+          <p>
+            Kontakt: <br />
+            Telefon: +43 1 234 567 890 <br />
+            E-Mail: kontakt@technikumhotel.at
+          </p>
+          <p>
+            Registereintrag: <br />
+            Eintragung im Handelsregister. <br />
+            Registernummer: HRB 987654 <br />
+            Registergericht: Handelsgericht Wien
+          </p>
+          <p>
+            Umsatzsteuer-ID: <br />
+            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz: ATU12345678
+          </p>
+          <p>
+            Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV: <br />
+            Franz Müller <br />
+            Mariahilfer Straße 123 <br />
+            1060 Wien, Österreich
+          </p>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
