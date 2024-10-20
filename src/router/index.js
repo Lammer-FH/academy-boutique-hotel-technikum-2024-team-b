d54d06415360f17f221702be5a367539f12bfbdf
David Desktop <david_ala@icloud.com>
2024-10-20 08:55:27 +0200

    changed to always scroll to top

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,9 @@ import GettingHereView from "@/views/GettingHereView.vue"
 
 
 const router = createRouter({
+    scrollBehavior () {
+        return { top: 0, left: 0 }
+    },
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
