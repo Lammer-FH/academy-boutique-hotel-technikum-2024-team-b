7cac29bd0f96587e7c94b4b6c746734d587b3c16
Magda <pa23l003@technikum-wien.at>
2024-11-03 10:21:53 +0100

    empty components and views created

---
+++ src/components/CollapseAvailability.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "CollapseAvailability"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RoomExtras.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "RoomExtras"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
