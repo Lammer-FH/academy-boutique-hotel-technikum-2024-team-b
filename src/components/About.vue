fb176468111838f4a3aba5869f55d1f6f98f5f14
Magda <pa23l003@technikum-wien.at>
2024-11-03 09:44:04 +0100

    added carousel

--- src/components/About.vue
+++ src/components/About.vue
@@ -11,8 +11,24 @@ export default {
       <b-col sm="12" md="8" lg="6">
         <h1>Boutique-Hotel Technikum</h1><br><br>
 
-        <b-img src="/images/About/hotel_lobby.webp" alt="HotelLobby"
-               class="img-fluid pb-5"></b-img>
+        <div id="carouselAbout" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="/images/About/hotel_lobby.webp" class="d-block w-100" alt="Hotel Lobby">
+            </div>
+            <div class="carousel-item">
+              <img src="/images/About/hotel_outside_carousel.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselAbout" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselAbout" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div><br><br>
 
         <p>Willkommen im <b>Boutique-Hotel Technikum</b>, Ihrem urbanen Rückzugsort im Herzen von Wien!
           Unser charmantes Hotel vereint zeitlose Eleganz mit modernem Komfort und bietet eine einzigartige
