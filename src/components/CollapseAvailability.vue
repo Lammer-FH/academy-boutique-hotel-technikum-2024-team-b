e3ebd9cba7b23cf749a045e31d84c343d8d72c83
Magda <pa23l003@technikum-wien.at>
2024-11-04 13:48:24 +0100

    collapse added

--- src/components/CollapseAvailability.vue
+++ src/components/CollapseAvailability.vue
@@ -1,10 +1,37 @@
 <script>
+
 export default {
-  name: "CollapseAvailability"
+  name: "CollapseAvailability",
+
+
+  data() {
+    return {
+      selected_date: '',
+      context: null,
+      isCollapsed: false
+    }
+  },
+  methods: {
+    changeVisibilityCollapse() {
+      this.isCollapsed = !this.isCollapsed;
+    },
+  }
 }
 </script>
 
 <template>
+  <div>
+    <div>
+      <div class="mb-3">
+        <b-button @click="changeVisibilityCollapse" class="m-1">Verfügbarkeit</b-button>
+        <b-collapse v-model="isCollapsed">
+          <b-card>Bitte wählen Sie ein Datum aus.
+
+          </b-card>
+        </b-collapse>
+      </div>
+    </div>
+  </div>
 
 </template>
 
