d2c3c03c6264750706af55ebed4caabb06a9401c
Magda <pa23l003@technikum-wien.at>
2024-11-09 20:46:00 +0100

    Check-Availability-Button, inputValidation and errormessage added

--- src/components/CollapseAvailability.vue
+++ src/components/CollapseAvailability.vue
@@ -11,14 +11,56 @@ export default {
       isCollapsed: false,
       arrival_date: '',
       departure_date: '',
+      errormessage: '',
+      validInput: true
     }
   },
   methods: {
     changeVisibilityCollapse() {
+      this.errormessage = '';
+      this.arrival_date = '';
+      this.departure_date = '';
       this.isCollapsed = !this.isCollapsed;
     },
+
+    validateInput() {
+      const currentDate = new Date();
+      const formattedDate = currentDate.toISOString().slice(0, 10);
+      this.validInput = true;
+      this.errormessage = '';
+
+      if (this.arrival_date < formattedDate) {
+        this.validInput = false;
+        this.errormessage = "Das Datum kann nicht in der Vergangenheit liegen."
+        return false
+      }
+
+      if (this.arrival_date === '' || this.departure_date === '') {
+        this.validInput = false;
+        this.errormessage = "Bitte geben Sie ein Datum ein.";
+        return false
+      }
+      if (this.departure_date <= this.arrival_date) {
+        this.validInput = false;
+        this.errormessage = "Abreisedatum muss nach dem Anreisedatum liegen!";
+        return false
+      }
+      return true
+    },
+
+
+    checkAvailability() {
+      {
+        if (this.validateInput()) {
+          //ajax-call zum Prüfen
+          console.log(this.arrival_date);
+        }
+      }
+    }
   }
 }
+
+
 </script>
 
 <template>
@@ -27,25 +69,30 @@ export default {
       <b-container fluid class="text-center">
         <b-row class="justify-content-center">
           <b-col sm="12" md="8" lg="6">
-            <b-button @click="changeVisibilityCollapse" variant="primary">Verfügbarkeit prüfen</b-button>
-        <b-collapse class="mx-5" v-model="isCollapsed">
-          <b-card class="mx-5">
-           Bitte wählen Sie ein Datum aus:<br>
-            <p>Anreise: <input type="date" v-model="arrival_date" />
-            Abreise: <input type="date" v-model="departure_date" />
-            </p>
+            <b-button @click="changeVisibilityCollapse" class="m-5">Verfügbarkeit prüfen</b-button>
+            <b-collapse v-model="isCollapsed">
+              <b-card>
+                <b>Bitte wählen Sie ein Datum aus:</b><br>
+                <p class="error" v-if="!validInput">{{ errormessage }}</p>
+                <p>Anreise: <input class="m-2" type="date" v-model="arrival_date"/><br>
+                  Abreise: <input class="m-2" type="date" v-model="departure_date"/><br>
+                </p>
+                <b-button variant="primary" v-on:click="checkAvailability">Verfügbarkeit prüfen</b-button>
 
-          </b-card>
-        </b-collapse>
+              </b-card>
+            </b-collapse>
           </b-col>
         </b-row>
       </b-container>
-      </div>
     </div>
+  </div>
 
 
 </template>
 
-<style scoped>
 
+<style scoped>
+.error {
+  color: red;
+}
 </style>
