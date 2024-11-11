21490564e51efd82df5ed01356525277d6fec878
Magda <pa23l003@technikum-wien.at>
2024-11-11 15:00:55 +0100

    two modals for room available and room not available created

---
+++ src/components/ModalRoomAvailable.vue
@@ -0,0 +1,50 @@
+<script>
+import AppModal from "@/components/nicht verwendet/AppModal.vue";
+import router from "@/router";
+
+export default {
+  name: "ModalRoomAvailable",
+
+  props: {
+    modelValue: Boolean,
+  },
+  data() {
+    return {
+      message: 'Das Zimmer ist zum ausgewählten Zeitpunkt verfügbar',
+    }
+  },
+
+
+  computed: {
+    isVisible: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value); // Sync with parent
+      },
+    },
+  },
+
+  methods: {
+    handleOk() {
+      this.modalShow = false;
+      router.push('home');
+    },
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <b-modal v-model="isVisible" title="Verfügbarkeit" ok-only ok-title="Jetzt Buchen" @ok="handleOk">{{ message }}
+
+    </b-modal>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
---
+++ src/components/ModalRoomNotAvailable.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: "ModalRoomNotAvailable",
+
+  props: {
+    modelValue: Boolean,
+  },
+  data() {
+    return {
+      message: 'Das Zimmer ist zum ausgewählten Zeitpunkt leider nicht verfügbar. Wählen Sie ein anderes Zimmer oder einen anderen Zeitpunkt',
+    }
+  },
+
+  computed: {
+    isVisible: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value); // Sync with parent
+      },
+    },
+  },
+
+  methods: {
+    handleOk() {
+      this.modalShow = false;
+      router.push('home');
+    },
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <b-modal v-model="isVisible" title="Verfügbarkeit" ok-only ok-title="Jetzt Buchen" @ok="handleOk">{{ message }}
+
+    </b-modal>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
