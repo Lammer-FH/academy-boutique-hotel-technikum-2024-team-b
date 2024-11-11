511a6ef729ce6256c8028983387258c328ae1adb
Magda <pa23l003@technikum-wien.at>
2024-11-11 14:59:40 +0100

    reusable modal created

---
+++ src/components/nicht verwendet/AppModal.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "AppModal",
+
+  props: {
+    modelValue: Boolean,
+    message: String,
+
+  },
+
+  computed: {
+    isVisible: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value); // Sync with parent
+      },
+    },
+  },
+
+}
+</script>
+
+
+<template>
+  <div>
+    <b-modal v-model="isVisible">{{ message }}
+
+    </b-modal>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
