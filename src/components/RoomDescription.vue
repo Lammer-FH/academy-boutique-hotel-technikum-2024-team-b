9ed14195f1cbeab0f38ab3d17667a5b5418680e1
Magda <pa23l003@technikum-wien.at>
2024-11-11 22:26:33 +0100

    RoomDescription-component created, RoomDetailAndAvailabilityView container, row and col added, content for RoomDetailsBasicInfo created

---
+++ src/components/RoomDescription.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+  name: "RoomDescription",
+
+  props: {
+    id: Number
+  }
+}
+</script>
+
+<template>
+  <!-- DUMMY-TEXTE -> müssen noch ausgetauscht werden -->
+  <p v-if="id===1">
+    Room1: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===2">
+    Room 2: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+  <p v-if="id===3">
+    Room 3: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===4">
+    Room 4: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===5">
+    Room 5: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===6">
+    Room 6: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===7">
+    Room 7: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===8">
+    Room 8: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===9">
+    Room 9: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+    ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+    Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+    für Ihren Aufenthalt.</p>
+
+  <p v-if="id===10">
+  Room 10: Unser stilvolles Boutique-Zimmer vereint Komfort und Charme mit einzigartigem Design. Helle, freundliche Farben und
+  ausgewählte Details schaffen eine warme Atmosphäre, während hochwertige Betten für erholsamen Schlaf sorgen.
+  Genießen Sie moderne Annehmlichkeiten, ein elegantes Badezimmer und einen Blick ins Grüne – der perfekte Rückzugsort
+  für Ihren Aufenthalt.</p>
+
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/RoomDetailAndAvailabilityView.vue
+++ src/views/RoomDetailAndAvailabilityView.vue
@@ -1,19 +1,23 @@
 <script>
 import CollapseAvailability from "@/components/CollapseAvailability.vue";
 import RoomDetailsBasicInfo from "@/components/RoomDetailsBasicInfo.vue";
-import IconTextRoomExtra from "@/components/icons/IconTextRoomExtra.vue";
-import ExtraList from "@/components/nicht verwendet/ExtraList.vue";
 import RoomExtras from "@/components/RoomExtras.vue";
 
 export default {
   name: "RoomDetailAndAvailabilityView",
-  components: {RoomExtras, ExtraList, IconTextRoomExtra, RoomDetailsBasicInfo, CollapseAvailability}
+  components: {RoomExtras, RoomDetailsBasicInfo, CollapseAvailability}
 }
 </script>
 
 <template>
+  <b-container fluid class="text-center">
+    <b-row class="justify-content-center">
+      <b-col sm="12" md="8" lg="6">
   <RoomDetailsBasicInfo/>
   <CollapseAvailability/>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <style scoped>
