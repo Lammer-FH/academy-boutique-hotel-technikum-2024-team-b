3134269bc402cf772e228151474f488497308edb
Magda <pa23l003@technikum-wien.at>
2024-11-11 19:20:09 +0100

    child-component for icon-Text created

--- src/components/RoomExtras.vue
+++ src/components/RoomExtras.vue
@@ -1,13 +1,22 @@
 <script>
 export default {
-  name: "RoomExtras"
+  name: "RoomExtras",
+
+
+  props: {
+    icon: String,
+    name: String,
+  }
 }
+
+
 </script>
 
 <template>
-
+  <img :src="icon" height="20" width="20" alt="dekorativer Icon"/> {{ name }}<br>
 </template>
 
 <style scoped>
 
 </style>
+
