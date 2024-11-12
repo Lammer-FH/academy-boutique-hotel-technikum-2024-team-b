b681eacf83c21a613e2bce379c24794eeebaf532
tamar <tamar.dono@gmail.com>
2024-11-12 21:05:27 +0100

    added path for room booking and conformation

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 import LandingpageView from "@/views/LandingpageView.vue";
 import ImpressumView from "@/views/ImpressumView.vue";
 import AboutView from "@/views/AboutView.vue";
-import GettingHereView from "@/views/GettingHereView.vue"
+import GettingHereView from "@/views/GettingHereView.vue";
+import ZimmerbuchenView from "@/views/ZimmerbuchenView.vue";
+import BuchungsbesteatigungView from "@/views/BuchungsbesteatigungView.vue";
 
-
+// Create the router instance
 const router = createRouter({
-    scrollBehavior () {
-        return { top: 0, left: 0 }
+    scrollBehavior() {
+        return { top: 0, left: 0 };
     },
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -31,8 +33,17 @@ const router = createRouter({
             name: 'gettingHere',
             component: GettingHereView
         },
-
+        {
+            path: '/zimmerbuchen',
+            name: 'zimmerbuchen',
+            component: ZimmerbuchenView
+        },
+        {
+            path: '/buchungsbestaetigung',
+            name: 'buchungsbestaetigung',
+            component: BuchungsbesteatigungView
+        }
     ]
-})
+});
 
-export default router
+export default router;
