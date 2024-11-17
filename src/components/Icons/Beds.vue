f31114c9487675e0719aa969008f175666f40157
David Desktop <david_ala@icloud.com>
2024-11-17 16:32:47 +0100

    deleted nicht verwendet folder and created icons

---
+++ src/components/Icons/Beds.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "Beds"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Icons/Handicapped.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "Handicapped"
+}
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
