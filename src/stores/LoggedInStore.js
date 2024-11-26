39e7949adfa4d4b46973175ced21747a9f2d8b74
David Ala MacBook <ala_david@icloud.com>
2024-11-26 20:12:16 +0100

    first draft of the loginHandlerStore

---
+++ src/stores/LoggedInStore.js
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useLoggedInStore = defineStore('LoggedIn', {
+    state: () => ({
+        firstname: "",
+        lastname: "",
+        birthDate: "",
+        emailAdresse: "",
+    }),
+
+    actions: {
+        async getUser() {
+            const apiUrl = 'https://boutique-hotel.helmuth-lammer.at/api/user';
+            const token = localStorage.getItem("token");
+
+            try {
+                console.log("Fetching user data...");
+                const response = await axios.get(apiUrl, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                console.log("Response received:", response.data);
+
+                let { firstname, lastname, birthDate, emailAdresse } = response.data;
+                this.firstname = firstname;
+                this.lastname = lastname;
+                this.birthDate = birthDate;
+                this.emailAdresse = emailAdresse;
+
+            } catch (error) {
+                console.error("Error fetching user data:", error.response ? error.response.data : error.message);
+            }
+        }
+    },
+});
